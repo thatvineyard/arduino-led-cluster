e424d564e7907e1f6c3c5d572fb2e6ba506c34ea
Carl Wingårdh <c.a.wingardh@gmail.com>
2019-01-02 14:35:37 +0100

    Added comments to Commands.h

--- src/Unit/Commands.h
+++ src/Unit/Commands.h
@@ -1,6 +1,15 @@
 #ifndef COMMANDS_H_DEFINED
 #define COMMANDS_H_DEFINED
 
+/**
+ *
+ * Settings are changes that are made once and don't interrupt macros.
+ *
+ * Macros are ongoing changes that will keep going until another macro or the
+ * STOP command is issued.
+ *
+ */
+
 enum Command {
   // SPECIAL COMMANDS
   NULL_COMMAND = -1,
@@ -15,15 +24,40 @@ enum Command {
   M_SINGLEFLASH
 };
 
+/**
+ * Sets a new macro. This will flag that a new macro is ready to be initialized.
+ */
 void setMacro(Command newCommand);
 
+/**
+ * Sets a new setting. This will flag that a new setting is ready to be applied.
+ */
 void setSetting();
 
+/**
+ * Looks at the current macro and performs that macro's init-function.
+ * Should only be called when a new function has been set.
+ */
 void initMacro();
 
+/**
+ * Looks at the current macro and performs that macro's tick-function. These
+ * tick functions are parameter-less and therefore don't have to worry about
+ * keeping track of the global list of parameters.
+ */
 void tickMacro();
 
-/** M_PULSE
+/**
+ * looks at the current setting and applies that setting.
+ * Should only be called when a new setting has been set.
+ */
+void applySetting();
+
+// MACROES
+
+/**
+ * M_PULSE
+ *
  * Turns on and off the LED with a fade. Duration and fade time is decided by
  * parameters. If off_duration and fade_out_duration are omitted they will be
  * the same as on_duration and fade_in_duration. If on_duration is omitted it
@@ -38,16 +72,34 @@ void init(String on_duration,
 void tick();
 }  // namespace m_pulse
 
+/**
+ * M_FLICKER
+ *
+ * Very quickly changes the brightness of the LED in a way that emulates a
+ * candle. Since this macro does not set any colors, the base color should be
+ * set to something like (255, 50, 0) for the best candle effect.
+ */
 namespace m_flicker {
 void init(String flicker_delay);
 void tick();
 }  // namespace m_flicker
 
+/**
+ * M_SOLID
+ *
+ * Statically lights the LED in the base color and brightness.
+ */
 namespace m_solid {
 void init();
 void tick();
 }  // namespace m_solid
 
+/**
+ * M_SINGLEFLASH
+ *
+ * Turns on the LED instantly, waits the given flash_duration, then fades to 0
+ * brightness during fade_duration.
+ */
 namespace m_singleflash {
 void init(String flash_duration, String fade_duration);
 void tick();
